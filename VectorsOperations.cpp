c760859ffd804e10ed7c09c04bdc2a0b013ecdd4
aya ahmad gamal <71007442+Aya-gamal2211@users.noreply.github.com>
2023-10-27 02:28:44 +0200

    Vectors Operations getSum, getAverage, getMin

--- VectorsOperations.cpp
+++ VectorsOperations.cpp
@@ -11,12 +11,37 @@ double getSum(vector<double> &numbers){
     return sum;
 }
 
+double getAverage(vector<double> &numbers){
+    double average = getSum(numbers) / numbers.size();
+
+    return average;
+}
+
+
+
+double getMin(vector<double> &numbers){
+    double min = numbers[0];
+
+    for (int i = 1; i < static_cast<int>(numbers.size()) ; i++){
+
+        if (numbers[i]<min)
+            min = numbers[i];
+
+    }
+
+    return min;
+}
+
+
 int main(){
 
     vector<double> numbers = {10, 15.5, 20, 15, 25};
     double summationResult = getSum(numbers);
     cout << "The vector summation is " << summationResult << endl;
 
+    cout << "The minimum value is " << getMin(numbers) << endl;
+
+    cout <<"The average value is "<<getAverage(numbers) <<endl;
 
     return 0;
 }
